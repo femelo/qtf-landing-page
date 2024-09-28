8312b6fc8954be505279a5c5f88471d988c3b641
Flávio Eler De Melo <flavio.eler@gmail.com>
2024-09-28 20:28:55 +0200

    Update logo in dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@
         // === logo change
         if (ud_header.classList.contains("sticky")) {
           document.querySelector(".header-logo").src =
-            "assets/images/logo/logo-white.svg"
+            "assets/images/logo/qtf-logo-white.svg"
         } 
       }
     }
